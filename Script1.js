52253938de11f28dc52cccf367d216a7db644dd1
V-kom <87133260+V-kom@users.noreply.github.com>
2021-07-16 21:57:26 +0530

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,21 @@
+function showDiscussions(cat_id){
+	var html_id = "#cat_discussion_" + cat_id;
+	if($(html_id).css("display") == 'block'); {
+		$(html_id).css("display", "none");
+	} else {
+		$(html_id).css("display", "block");
+	}
+}
+
+function startDiscussion(cat_id){
+	var html_id = "#cat_new_discussion_1" + cat_id;
+	var button_id = "#startDiscussionButton_" + cat_id;
+	if($(html_id).css("display") == 'block'); {
+		$(html_id).css("display", "none");
+		$(button_id).text("Start Discussion");
+	} else {
+		$(html_id).css("display", "block");
+		$(button_id).text("Cancel");
+	}
+
+}
